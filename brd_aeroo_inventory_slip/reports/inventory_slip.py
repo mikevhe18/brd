d67b0a60462cd4bc5d652c08b875fc9517324914
Andhitia Rama <andhitia.r@gmail.com>
2015-11-22 11:10:51 +0700

    Greenify travis

--- brd_aeroo_inventory_slip/reports/inventory_slip.py
+++ brd_aeroo_inventory_slip/reports/inventory_slip.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
-#    
-#    Copyright (C) 2011 OpenERP Italian Community (<http://www.openerp-italia.org>). 
-#    All Rights Reserved
+# 
+#    Author : Andhitia Rama, Michael Viriyananda, Nurazmi
+#    Copyright (C) 2015 OpenSynergy Indonesia
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -21,14 +21,13 @@
 
 from report import report_sxw
 
+
 class Parser(report_sxw.rml_parse):
 
     def __init__(self, cr, uid, name, context):
         super(Parser, self).__init__(cr, uid, name, context)
         self.data_list = []
-        self.localcontext.update({
-                                    'get_address' : self.get_address,
-                                    })
+        self.localcontext.update({'get_address': self.get_address})
 
     def get_product_template(self):
         # obj_picking = self.pool.get('stock.picking')
@@ -47,16 +46,16 @@ class Parser(report_sxw.rml_parse):
             template_id = a[0]
             template = obj_template.browse(self.cr, self.uid, [template_id])[0]
             dict_1 = {
-                    'template_id': template.id,
-                    'template_name': template.name,
-                    'moves': [],
-                    }
-            criteria = [('picking_id','=',picking_id),('product_id.product_tmpl_id','=',template_id)]
+                'template_id': template.id,
+                'template_name': template.name,
+                'moves': [],
+            }
+            criteria = [('picking_id','=', picking_id),('product_id.product_tmpl_id','=', template_id)]
             move_ids = obj_move.search(self.cr, self.uid, criteria)
             no = 1
             for move in obj_move.browse(self.cr, self.uid, move_ids):
                 dict_2 = {
-                        'no' : no,
+                        'no': no,
                         'product_name': move.product_id.display_name,
                         'uom': move.product_uom.name,
                         'qty': move.product_uom_qty,
