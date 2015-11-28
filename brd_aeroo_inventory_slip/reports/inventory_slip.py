a404d4765769c48d14c526c688e5ddc037342c5a
Andhitia Rama <andhitia.r@gmail.com>
2015-11-28 21:25:11 +0700

    Fixing import module on inventory_slip.py

--- brd_aeroo_inventory_slip/reports/inventory_slip.py
+++ brd_aeroo_inventory_slip/reports/inventory_slip.py
@@ -19,10 +19,10 @@
 #
 ##############################################################################
 
-from report import report_sxw
+from openerp.report.report_sxw import rml_parse
 
 
-class Parser(report_sxw.rml_parse):
+class Parser(rml_parse):
 
     def __init__(self, cr, uid, name, context):
         super(Parser, self).__init__(cr, uid, name, context)
