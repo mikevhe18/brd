3436dd9c2d5a64974ae804f9f5cb0c1d88ce42c1
mike <mike@mike.(none)>
2016-01-15 17:20:41 +0700

    Fixing Issues #12

--- brd_pos_receipt/static/src/js/brd_pos_receipt.js
+++ brd_pos_receipt/static/src/js/brd_pos_receipt.js
@@ -9,12 +9,76 @@ function brd_pos_receipt(instance, module){
             loaded = loaded.then(function(){
                 return self.fetch(
                     'res.company',
-                    ['logo'],
+                    [
+                        'currency_id',
+                        'email',
+                        'website',
+                        'company_registry',
+                        'vat',
+                        'name',
+                        'phone',
+                        'partner_id',
+                        'country_id',
+                        'tax_calculation_rounding_method',
+                        'logo'
+                    ],
                     {}
                 );
 
             }).then(function(companies){
                 self.company = companies[0];
+                
+                return self.fetch(
+                    'pos.config',
+                    ['stock_location_id'],
+                    [['id','=', self.pos_session.config_id[0]]]
+                );
+            }).then(function(configs){
+                self.config = configs[0];
+                self.config.iface_print_via_proxy2 = self.config.iface_print_via_proxy;
+
+                self.config.use_proxy = self.config.iface_payment_terminal ||
+                                        self.config.iface_electronic_scale ||
+                                        self.config.iface_print_via_proxy  ||
+                                        self.config.iface_scan_via_proxy   ||
+                                        self.config.iface_cashdrawer;
+                                        
+                return self.fetch(
+                    'stock.location',
+                    ['name','partner_id','id'],
+                    [['id', '=', self.config.stock_location_id[0]]]
+                );
+                
+            }).then(function(locations){
+                self.shop = locations[0];
+                
+                return self.fetch(
+                    'res.partner',
+                    [
+                        'id',
+                        'name',
+                        'street',
+                        'street2',
+                        'city',
+                        'state_id'
+                    ],
+                    [['id', '=', self.shop.partner_id[0]]]
+                );
+            
+            }).then(function(partner){
+                self.partner_location = partner[0];
+                
+                return self.fetch(
+                    'res.country.state',
+                    [
+                        'id',
+                        'name'
+                    ],
+                    [['id', '=', self.partner_location.state_id[0]]]
+                );
+                
+            }).then(function(state){
+                self.state = state[0];
             })
             return loaded;
         },
